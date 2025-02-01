eb7813d2e00b45d29c11335bc571f623dedcac45
Chirayu18 <chirayu.gupta@gmail.com>
2025-02-01 03:58:26 +0100

    Removed sudo commands from install script

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ echo "================ Installing zoxide ========="
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 echo "================ Installing nvim ========="
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo rm -rf ~/nvim
-sudo tar -C ~/ -xzf nvim-linux-x86_64.tar.gz
+rm -rf ~/nvim
+tar -C ~/ -xzf nvim-linux-x86_64.tar.gz
 echo "export PATH=\"$PATH:/opt/nvim-linux-x86_64/bin\"" >> ~/.bashrc
 echo "alias vi=nvim" >> ~/.bashrc
 echo "================ Configuring nvim ========="
